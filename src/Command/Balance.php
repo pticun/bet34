285caa8cb39b21be7d533703bf7087f7426aab4f
Florian <florian@gensdeconfiance.fr>
2018-05-27 15:06:09 +0200

    adjust balance

--- src/Command/Balance.php
+++ src/Command/Balance.php
@@ -48,7 +48,7 @@ class Balance extends Command
         $output->writeln(sprintf('Nombre de paris gagnés : %d (%d %%)', $nbWonBet, $percentageWonBet));
 
         // Balance
-        $output->writeln(sprintf('Balance : 1€ ==> %f€', 1 + $this->balanceService->getBalance($day)));
+        $output->writeln(sprintf('Balance : 1€ ==> %f€', number_format(1 + $this->balanceService->getBalance($day), 2)));
     }
 
     private function getDay(): DateTime
